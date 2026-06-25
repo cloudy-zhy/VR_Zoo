7ff9b576479d0cffe10ccff8a80b2d8b95683b43
cloudy-zhy <3094681978@qq.com>
2026-06-25 17:04:13 +0800

    更新场景1

--- Assets/Scripts/Manager/FruitManager.cs
+++ Assets/Scripts/Manager/FruitManager.cs
@@ -65,9 +65,9 @@ public class FruitManager : MonoBehaviour
 
     private IEnumerator LevelSwitchDelay()
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(2.0f);
         LoadLevel(currentLevelIndex);
-        DialogueController.Instance.ShowDialogueWithIndex();
+        if(currentLevelIndex<=3 || currentLevelIndex>=levels.Count) DialogueController.Instance.ShowDialogueWithIndex();
         // 原代码：if (currentLevelIndex > 3)
         // 修改为：没有下一关时才判定全部通关
         if (currentLevelIndex >= levels.Count)
